2718ae4273a9c13fc8461320c6b8503b5ee58dd8
darren <764190362@qq.com>
2017-09-30 08:35:01 +0800

    在视图中创建不同路由的方式

--- Startup.cs
+++ Startup.cs
@@ -127,7 +127,27 @@ namespace AspNetCoreRoutesExamples
             //);
 
             //属性路由
-            app.UseMvcWithDefaultRoute();
+            //app.UseMvcWithDefaultRoute();
+
+
+            //------------------------------
+            // 高级路由
+            //------------------------------
+            app.UseMvc(routes => {
+                //routes.MapRoute(
+                //    name:"NewRoute",
+                //    template:"App/Do{action}",
+                //    defaults:new { controller="Home"});
+
+                routes.MapRoute(
+                    name:"default",
+                    template:"{controller=Home}/{action=Index}/{id?}");
+
+
+                routes.MapRoute(
+                    name:"out",
+                    template:"outbound/{controller=Home}/{action=Index}");
+            });
         }
     }
 }
